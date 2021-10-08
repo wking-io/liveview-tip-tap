2707aebd78feddc456312cb2606a760c902ca58d
Will King <contact@wking.dev>
2021-10-08 10:59:17 -0500

    Updated the second chapter of John

---
+++ lib/without_ceasing/bible/data/john/two.ex
@@ -0,0 +1,157 @@
+defmodule Bible.Data.John.One do
+  def get() do
+    %{
+      book: "John",
+      number: 2,
+      structure: [
+        "The Wedding at Cana",
+        %{
+          id: 43_001_001,
+          number: 1,
+          text:
+            "On the third day there was a wedding at Cana in Galilee, and the mother of Jesus was there."
+        },
+        %{
+          id: 43_001_002,
+          number: 2,
+          text: "Jesus also was invited to the wedding with his disciples."
+        },
+        %{
+          id: 43_001_003,
+          number: 3,
+          text: "When the wine ran out, the mother of Jesus said to him, \"They have no wine.\""
+        },
+        %{
+          id: 43_001_004,
+          number: 4,
+          text:
+            "And Jesus said to her, \"Woman, what does this have to do with me? My hour has not yet come.\""
+        },
+        %{
+          id: 43_001_005,
+          number: 5,
+          text: "His mother said to the servants, \"Do whatever he tells you.\""
+        },
+        %{
+          id: 43_001_006,
+          number: 6,
+          text:
+            "Now there were six stone water jars there for the Jewish rites of purification, each holding twenty or thirty gallons."
+        },
+        %{
+          id: 43_001_007,
+          number: 7,
+          text:
+            "Jesus said to the servants, \"Fill the jars with water.\" And they filled them up to the brim."
+        },
+        %{
+          id: 43_001_008,
+          number: 8,
+          text:
+            "And he said to them, \"Now draw some out and take it to the master of the feast.\" So they took it."
+        },
+        %{
+          id: 43_001_009,
+          number: 9,
+          text:
+            "When the master of the feast tasted the water now become wine, and did not know where it came from (though the servants who had drawn the water knew), the master of the feast called the bridegroom"
+        },
+        %{
+          id: 43_001_010,
+          number: 10,
+          text:
+            "and said to him, \"Everyone serves the good wine first, and when people have drunk freely, then the poor wine. But you have kept the good wine until now.\""
+        },
+        %{
+          id: 43_001_011,
+          number: 11,
+          text:
+            "This, the first of his signs, Jesus did at Cana in Galilee, and manifested his glory. And his disciples believed in him."
+        },
+        %{
+          id: 43_001_012,
+          number: 12,
+          text:
+            "After this he went down to Capernaum, with his mother and his brothers and his disciples, and they stayed there for a few days."
+        },
+        "Jesus Cleanses the Temple",
+        %{
+          id: 43_001_013,
+          number: 13,
+          text: "The Passover of the Jews was at hand, and Jesus went up to Jerusalem."
+        },
+        %{
+          id: 43_001_014,
+          number: 14,
+          text:
+            "In the temple he found those who were selling oxen and sheep and pigeons, and the money-changers sitting there."
+        },
+        %{
+          id: 43_001_015,
+          number: 15,
+          text:
+            "And making a whip of cords, he drove them all out of the temple, with the sheep and oxen. And he poured out the coins of the money-changers and overturned their tables."
+        },
+        %{
+          id: 43_001_016,
+          number: 16,
+          text:
+            "And he told those who sold the pigeons, \"Take these things away; do not make my Father's house a house of trade.\""
+        },
+        %{
+          id: 43_001_017,
+          number: 17,
+          text:
+            "His disciples remembered that it was written, \"Zeal for your house will consume me.\""
+        },
+        %{
+          id: 43_001_018,
+          number: 18,
+          text: "So the Jews said to him, \"What sign do you show us for doing these things?\""
+        },
+        %{
+          id: 43_001_019,
+          number: 19,
+          text:
+            "Jesus answered them, \"Destroy this temple, and in three days I will raise it up.\""
+        },
+        %{
+          id: 43_001_020,
+          number: 20,
+          text:
+            "The Jews then said, \"It has taken forty-six years to build this temple, and will you raise it up in three days?\""
+        },
+        %{
+          id: 43_001_021,
+          number: 21,
+          text: "But he was speaking about the temple of his body."
+        },
+        %{
+          id: 43_001_022,
+          number: 22,
+          text:
+            "When therefore he was raised from the dead, his disciples remembered that he had said this, and they believed the Scripture and the word that Jesus had spoken."
+        },
+        "Jesus Knows What Is in Man",
+        %{
+          id: 43_001_023,
+          number: 23,
+          text:
+            "Now when he was in Jerusalem at the Passover Feast, many believed in his name when they saw the signs that he was doing."
+        },
+        %{
+          id: 43_001_024,
+          number: 24,
+          text:
+            "But Jesus on his part did not entrust himself to them, because he knew all people"
+        },
+        %{
+          id: 43_001_025,
+          number: 25,
+          text:
+            "and needed no one to bear witness about man, for he himself knew what was in man."
+        }
+      ]
+    }
+  end
+end
